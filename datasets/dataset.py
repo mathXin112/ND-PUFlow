6d133a6f0a97c1f063a15f7b1860296b04b50c23
Casey_Xin <32211985+mathXin112@users.noreply.github.com>
2023-05-29 21:21:36 +0800

    Update dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -21,14 +21,10 @@ def load_h5_data(h5_filename,num_point,up_ratio,skip_rate=1,use_randominput=Fals
             input = f['poisson_%d' % num_4X_point][:]
             gt = f['poisson_%d'% num_out_point][:]
     else:
-        # logging.info('do not use random input')
         with h5py.File(h5_filename, 'r') as f:
             input = f['poisson_%d' % num_point][:]
             gt = f['poisson_%d' % num_out_point][:]
     assert len(input) == len(gt)
-    # print("load_h5_input", input.shape)  ##(6900, 1024, 3)
-    # print("load_h5_gt", gt.shape) ##(6900,1024,3)
-    # logging.info("Normalize the data")
     data_radius = np.ones(shape=(len(input)))
     centroid = np.mean(input[:, :, 0:3], axis=1, keepdims=True)
     input[:, :, 0:3] = input[:, :, 0:3] - centroid
@@ -51,40 +47,20 @@ class PU1KDataset(Dataset):
     # def __init__(self, data_path,num_point,up_ratio,skip_rate=1,use_randominput=False,transform=False):
     def __init__(self, data_path, num_point, up_ratio, skip_rate=1, use_randominput=False): ##, transform=False): ##, type='train'):
         super().__init__()
-        # self.transform = transform
         self.num_point =num_point
         self.up_ratio = up_ratio
-        # self.use_randominput = use_randominput
-        # self.type = type ##胡鑫添加
-        # self.input_data, self.gt_data = load_h5_data(data_path,num_point,up_ratio,skip_rate, use_randominput)
         self.input_data, self.gt_data, self.radius_data = load_h5_data(data_path, num_point, up_ratio, skip_rate, use_randominput)
     def __len__(self):
         return len(self.input_data)
     def __getitem__(self, idx):
         input_data = self.input_data[idx]
-        # print("PU1K", input_data.shape)
         gt_data = self.gt_data[idx]
-        # radius_data = self.radius_data[idx]
-        # if self.use_randominput:
-        #     idx = point_operation.nonuniform_sampling(input_data.shape[0],sample_num=self.num_point)
-        #     input_data = input_data[idx]
-        # if self.transform:
-        #     input_data = point_operation.jitter_perturbation_point_cloud(input_data,
-        #                                                                        sigma=0.01,
-        #                                                                        clip=0.03)
-        #     input_data,gt_data = point_operation.rotate_point_cloud_and_gt(input_data,gt_data)
-        #     input_data,gt_data,scales = point_operation.random_scale_point_cloud_and_gt(input_data,gt_data,
-        #         scale_low=0.8,
-        #         scale_high=1.2)
-            # radius_data = radius_data * scales
         pair = {
             'pcl_low': torch.Tensor(input_data).clone(),
             'pcl_high': torch.Tensor(gt_data).clone(),
             # 'radius': radius_data.clone()
         }
-        # print("pair", pair.shape)
         return pair
-        # return input_data,gt_data,radius_data
 
 class PUNET_Dataset_Whole(Dataset):
     def __init__(self, data_dir='./PU1K/test/input_2048'):
